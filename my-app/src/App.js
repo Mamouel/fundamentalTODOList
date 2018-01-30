b46b6494720320a46cad4410accca261cc543f2c
mamouel <mamouel.dod@gmail.com>
2018-01-30 15:30:17 -0500

    question 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {number: 0};
+  }
+
+  componentDidMount() {
+    setInterval(() => this.setState({number: Math.floor(Math.random() * 1000)}), 1000)
+  }
+
   render() {
-    return 12;
+    return this.state.number
   }
 }
 
